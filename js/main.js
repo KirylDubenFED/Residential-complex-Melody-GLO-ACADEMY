2620850a5717eebf76779bb70bf04d7399bb876b
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-12-14 00:30:02 +0300

    add jQuery logic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-    
+    var currentFloor = 2;
+    var counterUp = $(".counter__up");
+    $(".home__image path").on("mouseover", function () {
+        currentFloor = $(this).attr("data-floor");
+        $(".counter").text(currentFloor);
+    });
+
+    counterUp.on("click", function () {
+        if (currentFloor < 18) {
+            currentFloor++;
+        usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2,
+        useGrouping: false });
+        $(".counter").text(usCurrentFloor);
+        $("[data-floor=4]").toggleClass("current-floor");
+        }
+    });
 });
